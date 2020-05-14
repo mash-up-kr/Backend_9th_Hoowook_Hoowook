d79e326220481de8050a98aa078342778501df4d
korea8378 <korea8378@naver.com>
2020-05-14 20:28:31 +0900

    init spring project

---
+++ src/main/java/mashup/backend/hoowook_hoowwok/HoowookHoowwokApplication.java
@@ -0,0 +1,13 @@
+package mashup.backend.hoowook_hoowwok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoowookHoowwokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoowookHoowwokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mashup/backend/hoowook_hoowwok/HoowookHoowwokApplicationTests.java
@@ -0,0 +1,13 @@
+package mashup.backend.hoowook_hoowwok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoowookHoowwokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
